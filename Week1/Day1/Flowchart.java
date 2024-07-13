67ddde2fea9a59d70bea48ca900cbf69d4d5dc42
prachii-byte <mishraprachi.2005@gmail.com>
2024-07-13 12:35:11 +0530

    first commit

---
+++ Week1/Day1/Flowchart.java
@@ -0,0 +1,13 @@
+package Week1.Day1;
+import java.util.Scanner;
+public class Flowchart {
+public static void main(String[] args){
+    //input
+    Scanner  sc=new Scanner(System.in);
+    int a= sc.nextInt();
+    //output
+    System.out.println("Value input by user is");
+    System.out.print(a);
+    sc.close();
+}
+}
